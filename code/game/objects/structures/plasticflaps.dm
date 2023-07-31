SinguloStation13/SinguloStation13-Archived

Plastic flap and holofirelock nerfs (#19)
* Holofirelock nerf

* Nerfs firedoors and plastic flaps

* Firedoor nerf didn't work...

* commend changes

* changes iscarbon check to ishuman check

Co-authored-by: TheOneAndMightyRed <41807890+TheOneAndMightyRed@users.noreply.github.com>

* Holofirelock buffed slightly and change to examine text

* Commend changes

Co-authored-by: TheOneAndMightyRed <41807890+TheOneAndMightyRed@users.noreply.github.com>

code/game/objects/structures/plasticflaps.dm
@@ -89,6 +89,8 @@
 
 	else if(isliving(A)) // You Shall Not Pass!
 		var/mob/living/M = A
+		if(ishuman(A)) //Singulo edit - nerfs holofans and plastic flaps
+			. = FALSE //Singulo edit - nerfs holofans and plastic flaps
 		if(isbot(A)) //Bots understand the secrets
 			return TRUE
 		if(M.buckled && istype(M.buckled, /mob/living/simple_animal/bot/mulebot)) // mulebot passenger gets a free pass.

MaltVinegar/SinguloStation13

code/game/objects/structures/plasticflaps.dm
@@ -90,6 +90,8 @@
 
 	else if(isliving(mover)) // You Shall Not Pass!
 		var/mob/living/living_mover = mover
+		if(ishuman(mover)) //Singulo edit - nerfs holofans and plastic flaps
+			return FALSE //Singulo edit - nerfs holofans and plastic flaps
 		if(isbot(mover)) //Bots understand the secrets
 			return TRUE
 		if(istype(living_mover.buckled, /mob/living/simple_animal/bot/mulebot)) // mulebot passenger gets a free pass.